4e2e603d9bbe1393fa3a017291cb8d6deb3e2bb3
Arthur Vianna <arthurpedrosa96.ap@gmail.com>
2022-05-17 10:35:02 -0300

    added players to pitch

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import Player from './components/player';
 function App() {
   const data = {
     formation: '4-4-3',
-    players: ['courtois', 'carvajal', 'militao', 'alaba', 'marcelo', 'kroos', 'casemiro', 'modric', 'rodrygo', 'benzema', 'vini jr']
+    players: [['courtois'], ['carvajal', 'militao', 'alaba', 'marcelo'], ['kroos', 'casemiro', 'modric'], ['rodrygo', 'benzema', 'vini jr']]
   }
+  
+  const formationSplit = data.formation.split('-');
+  const numberOfRows = formationSplit.length;
 
   return (
     <div className="App">
       <Header />
       <div className='pitch'>
         <div className='players'>
-          <Player playerName='courtois' />
+          {data.players.map((section) => (<div className='players-row' key={section}>
+            {section.map((player) => <Player playerName={player} key={player}/>)}
+          </div>))}
         </div>
       </div>
     </div>
--- src/components/player/index.jsx
+++ src/components/player/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import './player.css';
 import playerShirt from '../../assets/images/shirt.png';
@@ -6,6 +6,18 @@ import playerShirt from '../../assets/images/shirt.png';
 const Player = ({ playerName }) => {
   const [tries, setTries] = useState(null);
   const [correctName, setCorrectName] = useState(false);
+  const [shownText, setShownText] = useState('');
+
+  const transformNameToDots = (name) => {
+    let dots = '';
+    const nameArray = name.split('');
+    nameArray.forEach(() => dots += '*');
+    return dots;
+  }
+
+  useEffect(() => {
+    setShownText(correctName ? playerName : transformNameToDots(playerName));
+  }, [correctName, playerName]);
 
   return (
     <div className="player">
@@ -14,7 +26,7 @@ const Player = ({ playerName }) => {
         <p className="number-of-tries">{ tries ? tries : '?' }</p>
         <div className="player-name-length">{ playerName.length }</div>
       </div>
-      <input type={correctName ? 'text' : 'password'} className="player-name" value={playerName} />
+      <div className="player-name" >{ shownText }</div>
     </div>
   );
 }
